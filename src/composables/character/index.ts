a7a2cb078b1139765b69d64673672be77622cbfe
Peter Graif <peter@theopen.institute>
2024-12-04 11:54:54 +0545

    character interface touchup

--- src/composables/character/index.ts
+++ src/composables/character/index.ts
@@ -6,10 +6,6 @@ import type { Ref } from 'vue'
 import { watch } from 'vue'
 import type { Actor } from '@/types/pf2e-types'
 
-import { type Item } from './item'
-import { type Stat } from './stat'
-import { type Modifier } from './modifier'
-
 import { type CharacterCore, useCharacterCore } from './characterCore'
 import { type CharacterStats, useCharacterStats } from './characterStats'
 import { type CharacterResources, useCharacterResources } from './characterResources'
@@ -35,4 +31,10 @@ export function useCharacter(actor: Ref<Actor | undefined>) {
   return { character }
 }
 
-export type { Stat, Modifier, Item }
+import type { Item } from './item'
+import type { Stat } from './stat'
+import type { Modifier } from './modifier'
+import type { Strike } from './strike'
+import type { Equipment, SpellcastingEntry, Spell } from './characterItems'
+import type { Action } from './characterActions'
+export type { Item, Stat, Modifier, Strike, Equipment, SpellcastingEntry, Spell, Action }
