509ef336fe8409b99942751268fa82a4fb517989
Peter Graif <peter@theopen.institute>
2024-12-10 12:54:59 +0545

    adjust weight calculations for inventory

--- src/components/EquipmentListItem.vue
+++ src/components/EquipmentListItem.vue
@@ -6,13 +6,13 @@ const emits = defineEmits(['itemClicked'])
 const totalWeight = computed(() => {
   if (item?.system?.bulk?.value === 0 || item?.system?.stackGroup === 'coins') return '-'
   else if (item?.system?.bulk?.value < 1)
-    return item?.system?.bulk?.value * item?.system?.quantity * 10 + 'L'
-  else return (item?.system?.bulk?.value * item?.system?.quantity).toFixed(1)
+    return Math.floor(item?.system?.bulk?.value * item?.system?.quantity * 10) + 'L'
+  else return Math.floor(item?.system?.bulk?.value * item?.system?.quantity)
 })
 </script>
 <template>
   <a
-    class="flex cursor-pointer items-center justify-between gap-1"
+    class="grid cursor-pointer grid-cols-[auto_30px_20px] items-end gap-1"
     :class="{
       'text-gray-300': item.system?.equipped?.carryType === 'dropped',
       'ml-3': item?.system?.containerId
@@ -20,11 +20,18 @@ const totalWeight = computed(() => {
     @click="() => emits('itemClicked', item)"
   >
     <div class="truncate" :class="{ italic: item.type === 'backpack' }">{{ item.name }}</div>
-    <div class="flex justify-between gap-1">
-      <div class="text-xs">(x{{ item?.system?.quantity }})</div>
-      <div class="w-4 text-right text-xs">
-        {{ totalWeight }}
-      </div>
+    <!-- <div class="flex gap-1"> -->
+    <div class="text-right text-xs">(x{{ item?.system?.quantity }})</div>
+    <div
+      class="text-right text-xs"
+      :class="[
+        item?.system?.bulk?.value < 1 || item?.system?.stackGroup === 'coins'
+          ? 'font-light text-gray-600'
+          : 'font-semibold'
+      ]"
+    >
+      {{ totalWeight }}
     </div>
+    <!-- </div> -->
   </a>
 </template>
