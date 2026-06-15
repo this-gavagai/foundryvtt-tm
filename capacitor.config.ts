0e120fb711fd7b54dbbcbd7ce85b760cb6ecd073
Peter Graif <peter@theopen.institute>
2026-06-15 14:28:08 +0545

    first attempt at a capacitor build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli'
+
+const config: CapacitorConfig = {
+  appId: 'io.github.thisgavagai.tablemate',
+  appName: 'Tabula Mensa',
+  webDir: 'dist'
+}
+
+export default config
